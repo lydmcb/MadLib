45060cc8e987e14792baa83527f2cb5931f5236c
Lydia McBride <lydmcbride13@gmail.com>
2023-03-29 20:14:27 -0400

    Partially Complete madlibs

--- src/main/java/MadLibs.java
+++ src/main/java/MadLibs.java
@@ -1,54 +1,103 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Random;
 import java.util.Scanner;
 
 /*
  * Create a unique Mad Libs story!
  * https://www.madtakes.com/libs/093.html
  */
-public class MadLibs {
+public class MadLibs extends Random {
+
+    private final Scanner userInput = new Scanner(System.in);
+
+  /*  private final String[] verbs = {"lick", "sneak", "dive", "slay", "attack", "sprint"};
+    private final String[] actionVerbs = {"throwing", "fighting", "rolling", "growing","swimming","galloping"};
+    private final String[] adjectives = {"fierce", "smelly", "strange","feral", "magnificent", "appropriate" };
+    private final String[] jobs = {"poet", "lion tamer", "artist", "developer", "firefighter", "sandwich artist" };
+    private final String[] animals = {"aardvark", "dolphin", "snail", "lama", "sloth", "dodo bird"};
+    private final String[] nouns = {"fire", "bottle", "podcast", "child", "toaster", "shovel"};*/
+
 
     // 1. Make a main method that includes all the steps below
     public static void main(String[] args) {
+        MadLibs madLibs = new MadLibs();
+        madLibs.run();
 
-        // 2. Ask the player to enter a verb
+      // Random random = new Random();
 
-        // 3. Ask the player to enter a verb ending in 'ing'
-
-        // 4. Ask the player to enter a noun
-
-        // 5. Ask the player to enter an adjective
-
-        // 6. Ask the player to enter an occupation
-
-        // 7. Ask the player to enter an animal
-
-        // 8. Ask the player to enter an adjective
-
-        // 9. Ask the player to enter a verb ending in 'ing'
-
-        // 10. Ask the player to enter a noun
-
-        // The quotes below have been written as a group of Strings joined together by + signs.
-        // The story contains placeholders, indicated by [** **] which you need to replace with
-        // the values entered by the player.
-        // Note the \n characters represent newlines that move the following text onto the next line
-
-        String quotes = "'My name is [** 2. Verb **]; learn it well, for it`s the [** 3. Verb-ing **] sound of your [** 4. noun **].'\n" +
-                "'I told Killian I`d be [** 5. Adjective **]. I wouldn`t want to be a/an [** 6. Occupation **].'\n" +
-                "'I`m a cybernetic [** 7. animal **]: living tissue over a/an [** 8. adjective **] endoskeleton.'\n" +
-                "'If things have gone wrong, I`m [** 9. adjective **] myself, and you`ve got a wet [** 10. noun **] wrapped around your head.'\n" +
-                "- Arnold Schwarzenegger";
-
-        // 11. Print the quotes to the console to display the Mad Lib!
-        System.out.println(quotes);
-
-        /*
-         * EXTRA:
-         * Create a random word generator to allow the user to let the program
-         * select a random word.
-         */
     }
-}
+
+        public void run () {
+
+            System.out.println(
+                    "*******************\n" +
+                    "WELCOME TO MADLIBS!\n" +
+                    "*******************");
+            System.out.println();
+            System.out.println("Please respond to the prompts below, if you get stuck just hit enter and we'll help you out!");
+            System.out.println();
+
+
+
+            System.out.print("Please enter a verb >>> ");
+           // if(userInput.equals("")){
+              //  String firstVerb = random.nextInt(verbs.length);
+          //  } else {
+            String firstVerb = userInput.nextLine();
+
+
+            System.out.print("Now, enter a verb ending in 'ing' >>> ");
+            String ingVerb = userInput.nextLine();
+
+
+            System.out.print("How about a noun? >>> ");
+            String noun = userInput.nextLine();
+
+
+            System.out.print("Let's have a nice, juicy adjective >>> ");
+            String adjective = userInput.nextLine();
+
+
+            System.out.print("What's your dream job? >>> ");
+            String occupation = userInput.nextLine();
+
+
+            System.out.print("Tell me about your inner animal >>> ");
+            String animal = userInput.nextLine();
+
+
+            System.out.print("One more adjective, please >>> ");
+            String secondAdjective = userInput.nextLine();
+
+
+            System.out.print("And another 'ing' verb >>> ");
+            String actionVerb = userInput.nextLine();
+
+
+            System.out.print("Big finish! One more noun >>> ");
+            String lastNoun = userInput.nextLine();
+
+
+
+            String quotes = "'My name is " + firstVerb + "; learn it well, for it`s the " + ingVerb + " sound of your " + noun + ".'\n" +
+                    "'I told Killian I`d be " + adjective + ". I wouldn`t want to be a/an " + occupation + ".'\n" +
+                    "'I`m a cybernetic " + animal + ": living tissue over a/an " + secondAdjective + " endoskeleton.'\n" +
+                    "'If things have gone wrong, I`m " + actionVerb + " myself, and you`ve got a wet " + lastNoun + " wrapped around your head.'\n" +
+                    "- Arnold Schwarzenegger";
+
+           
+            System.out.println(quotes);
+
+
+
+
+            /*
+             * EXTRA:
+             * Create a random word generator to allow the user to let the program
+             * select a random word.
+             */
+        }
+    }
 
